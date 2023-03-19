3612158d09313670a8c0e1989a845d7f54eaa143
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-03-19 08:41:01 -0700

    grouping in Test.

--- ReactTestingWithJest/my-app/src/component/greet/greetTestDrivenApproach.test.tsx
+++ ReactTestingWithJest/my-app/src/component/greet/greetTestDrivenApproach.test.tsx
@@ -1,17 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import GreettestDrivenApproach from './greetTestDrivenApproach';
 
-test("Greet Render Correctly", () => {
-    render(<GreettestDrivenApproach />)
-    const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument();
-})
+//we can grouping thte test
+describe(GreettestDrivenApproach, () => {
+    test("Greet Render Correctly", () => {
+        render(<GreettestDrivenApproach />)
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+    })
 
-
-test("Greet Render Correctly", () => {
-    render(<GreettestDrivenApproach name='Wardha' />)
-    const textElement = screen.getByText("Hello Wardha");
-    expect(textElement).toBeInTheDocument();
+    test("Greet Render Correctly", () => {
+        render(<GreettestDrivenApproach name='Wardha' />)
+        const textElement = screen.getByText("Hello Wardha");
+        expect(textElement).toBeInTheDocument();
+    })
 })
 
 //test.only or test.skip these two are use for Filtering the test
+//test.only or test.skip these two are use for Filtering the test we can do this with describe also
